178fa6a2f2233e1b290c94069005dd9621691e86
Bernardo Biesseck <biesseck@gmail.com>
2023-10-01 14:02:23 -0300

    align_crop_faces_retinaface.py: load all image paths before starting face detection

--- align_crop_faces_retinaface.py
+++ align_crop_faces_retinaface.py
@@ -25,7 +25,7 @@ def getArgs():
     parser.add_argument('--gpu', default=-1, type=int, help='gpu id， when the id == -1, use cpu')
     parser.add_argument('--face_size', type=int, default=112, help='the size of the face to save, the size x%2==0, and width equal height')
     parser.add_argument('--thresh', type=float, default=0.5, help='threshold for face detection')
-    parser.add_argument('--scales', type=str, default=[1.0], help='the scale to resize image before detecting face')
+    parser.add_argument('--scales', type=str, default='[1.0]', help='the scale to resize image before detecting face')
     parser.add_argument('--draw_bbox_lmk', action='store_true', help='')
     args = parser.parse_args()
 
@@ -53,9 +53,19 @@ def draw_lmks(img, lmks):
     return result_img
 
 
+def get_all_files_in_path(folder_path, file_extension='.jpg'):
+    file_list = []
+    for root, _, files in os.walk(folder_path):
+        for filename in files:
+            if filename.endswith(file_extension):
+                file_list.append(os.path.join(root, filename))
+    file_list.sort()
+    return file_list
+
+
 def crop_align_face(args):
-    input_dir = args.input_path
-    output_dir = args.output_path
+    input_dir = args.input_path.rstrip('/')
+    output_dir = args.output_path.rstrip('/')
     if not os.path.exists(input_dir):
         print('the input path is not exists!')
         sys.exit()
@@ -67,8 +77,9 @@ def crop_align_face(args):
     else:
         ctx = mx.gpu(args.gpu)
 
-    # detector = RetinaFace('./model/R50', 0, gpuid, 'net3')
-    detector = RetinaFace('./retinaface/model/retinaface-R50/R50', 0, args.gpu, 'net3')
+    det_path = './retinaface/model/retinaface-R50/R50'
+    print(f'\nLoading face detector \'{det_path}\'...')
+    detector = RetinaFace(det_path, 0, args.gpu, 'net3')
 
     count_no_find_face = 0
     count_crop_images = 0
@@ -76,62 +87,64 @@ def crop_align_face(args):
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
 
-    for root, dirs, files in tqdm(os.walk(input_dir)):
-        for file_name in files:
+    ext = '.jpg'
+    print(f'\nSearching \'{ext}\' files in path \'{input_dir}\'...')
+    all_img_paths = get_all_files_in_path(input_dir, ext)
+    assert len(all_img_paths) > 0, f'Error: no files found with extention {ext} in path \'{input_dir}\''
+    # for i, img_path in enumerate(all_img_paths):
+    #     print(f'{i}/{len(all_img_paths)-1} - path: {img_path}')
+    # print('len(all_img_paths):', len(all_img_paths))
+    # sys.exit(0)
+
+    for i, input_img_path in enumerate(all_img_paths):
+        print(f'{i}/{len(all_img_paths)}\nReading {input_img_path} ...')
+        face_img = cv2.imread(input_img_path)
+
+        print(f'Detecting face...')
+        ret = detector.detect(face_img, args.thresh, args.scales, do_flip=False)
+
+        bbox, points = ret
+        if bbox.shape[0] == 0:
+            print('%s do not find face'%input_img_path)
+            count_no_find_face += 1
+            continue
+
+        confidences = [bbox[idx, 4] for idx in range(bbox.shape[0])]
+        print('Confidences:', confidences)
+
+        for bbox_idx in range(bbox.shape[0]):
+            bbox_ = bbox[bbox_idx, 0:4]
+            points_ = points[bbox_idx, :].reshape((5, 2))
+
+            print(f'Aligning and cropping to size {args.face_size}x{args.face_size} ...')
+            face = face_align.norm_crop(face_img, landmark=points_, image_size=args.face_size)
+
+            # face_name = '%s.png'%(file_name.split('.')[0])
+            output_img_path = input_img_path.replace(input_dir, output_dir)
+            face_name = output_img_path.split('/')[-1].split('.')[0] + '.png'
+            output_img_path = os.path.join(os.path.dirname(output_img_path), face_name)
+            os.makedirs(os.path.dirname(output_img_path), exist_ok=True)
             
-            if not file_name.split('.')[-1] == 'jpg' \
-                and not file_name.split('.')[-1] == 'jpeg' \
-                and not file_name.split('.')[-1] == 'png':
-                continue
+            if args.draw_bbox_lmk:
+                face_img_copy = draw_bbox(face_img, bbox_)
+                face_img_copy = draw_lmks(face_img_copy, points_)
+                face_name = '%s_bbox.png'%(input_img_path.split('/')[-1].split('.')[0])
+                file_path_bbox_save = os.path.join(os.path.dirname(output_img_path), face_name)
+                print(f'Saving {file_path_bbox_save}')
+                cv2.imwrite(file_path_bbox_save, face_img_copy)
 
-            output_root = root.replace(input_dir, output_dir)
-            if not os.path.exists(output_root):
-                os.makedirs(output_root)
+            print(f'Saving {output_img_path} ...')
+            cv2.imwrite(output_img_path, face)
+            print('-------------')
+            break   # take only the most confident face in image
 
-            file_path = os.path.join(root, file_name)
-            print(f'Reading {file_path}')
-            face_img = cv2.imread(file_path)
-            
-            print(f'Detecting face...')
-            ret = detector.detect(face_img, args.thresh, args.scales, do_flip=False)
-            
-            if ret is None:
-                print('%s do not find face'%file_path)
-                count_no_find_face += 1
-                continue
-            bbox, points = ret
-            if bbox.shape[0] == 0:
-                print('%s do not find face'%file_path)
-                count_no_find_face += 1
-                continue
+        count_crop_images += 1
 
-            for i in range(bbox.shape[0]):
-                bbox_ = bbox[i, 0:4]
-                # points_ = points[i, :].reshape((2, 5)).T
-                points_ = points[i, :].reshape((5, 2))
-
-                if args.draw_bbox_lmk:
-                    face_img_copy = draw_bbox(face_img, bbox_)
-                    face_img_copy = draw_lmks(face_img_copy, points_)
-                    face_name = '%s_bbox.png'%(file_name.split('.')[0])
-                    file_path_bbox_save = os.path.join(output_root, face_name)
-                    print(f'Saving {file_path_bbox_save}')
-                    cv2.imwrite(file_path_bbox_save, face_img_copy)
-
-                face = face_align.norm_crop(face_img, landmark=points_, image_size=args.face_size)
-
-                face_name = '%s.png'%(file_name.split('.')[0])
-                
-                file_path_save = os.path.join(output_root, face_name)
-                print(f'Saving {file_path_save}')
-                cv2.imwrite(file_path_save, face)
-                print('-------------')
-                break   # take only the most confident face in image
-
-            count_crop_images += 1
-
+    print('-------------------------------')
+    print('Finished')
     print('%d images crop successful!' % count_crop_images)
     print('%d images do not crop successful!' % count_no_find_face)
+
 
 if __name__ == '__main__':
     args = getArgs()
